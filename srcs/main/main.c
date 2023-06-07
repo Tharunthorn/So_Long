b11e761c9f8696fc2cb1ea16fc3677a7efd31c44
Tharunthorn Musik <tharunthornmusik@Tharunthorns-MacBook-Air.local>
2023-06-07 18:33:48 +0700

    modify main

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,7 +6,7 @@
 /*   By: tharunthornmusik <tharunthornmusik@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 18:02:41 by tharunthorn       #+#    #+#             */
-/*   Updated: 2023/06/07 17:53:10 by tharunthorn      ###   ########.fr       */
+/*   Updated: 2023/06/07 18:27:23 by tharunthorn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,10 @@ int	main(int argc, char **argv)
 {
 	t_game	game;
 	t_level	level;
-	char	*map_file;
 
 	if (argc != 2)
 		return (0);
-	map_file = (char *)malloc(sizeof(char) * ft_strlen(argv[1]) + 1);
-	if (!map_file)
-		return (0);
-	ft_strlcpy(map_file, argv[1], ft_strlen(argv[1]) + 1);
-	level = level_init(map_file);
+	level = level_init(argv[1]);
 	game = game_init(level);
 	game_run(game);
 	return (0);
