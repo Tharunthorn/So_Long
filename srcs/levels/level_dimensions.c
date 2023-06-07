b272fa066cf851d2422736345deaa2b8c822347b
Tharunthorn Musik <tharunthornmusik@Tharunthorns-MacBook-Air.local>
2023-06-07 18:23:44 +0700

    modify level_dimensions

--- srcs/levels/level_dimensions.c
+++ srcs/levels/level_dimensions.c
@@ -6,7 +6,7 @@
 /*   By: tharunthornmusik <tharunthornmusik@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 00:00:32 by tharunthorn       #+#    #+#             */
-/*   Updated: 2023/06/07 00:04:24 by tharunthorn      ###   ########.fr       */
+/*   Updated: 2023/06/07 17:46:37 by tharunthorn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,26 @@
 t_dimensions	level_dimensions_init(char *map_file)
 {
 	t_dimensions	dimensions;
+	int				fd;
+	char			ch;
 
-	dimensions.width = 20;
-	dimensions.height = 10;
+	dimensions.width = 0;
+	dimensions.height = 0;
+	fd = open(map_file, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_printf("map_file: %s\n", map_file);
+		ft_printf("Error: Could not open file.\n");
+		exit(1);
+	}
+	while (read(fd, &ch, 1) == 1)
+	{
+		if (ch == '\n')
+			dimensions.height++;
+		else if (dimensions.height == 0)
+			dimensions.width++;
+	}
+	close(fd);
 	return (dimensions);
 }
 
--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,7 +6,7 @@
 /*   By: tharunthornmusik <tharunthornmusik@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 18:02:41 by tharunthorn       #+#    #+#             */
-/*   Updated: 2023/06/07 00:31:08 by tharunthorn      ###   ########.fr       */
+/*   Updated: 2023/06/07 17:53:10 by tharunthorn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 		return (0);
-	map_file = (char *)malloc(sizeof(char) * ft_strlen(argv[1]));
+	map_file = (char *)malloc(sizeof(char) * ft_strlen(argv[1]) + 1);
 	if (!map_file)
 		return (0);
-	ft_strlcpy(map_file, argv[1], ft_strlen(argv[1]));
+	ft_strlcpy(map_file, argv[1], ft_strlen(argv[1]) + 1);
 	level = level_init(map_file);
 	game = game_init(level);
 	game_run(game);
